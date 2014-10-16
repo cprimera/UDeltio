c93b99112a4bb1772162cc15fad326b0f65b76e4
Christopher Primerano <cprimera@mac.com>
2014-10-16 19:28:46 -0400

    Added BaseSerializer class

---
+++ Phase2/Web/utils.py
@@ -0,0 +1,80 @@
+from flask import Flask
+
+import json
+
+
+
+class BaseSerializer(object):
+	fields = []
+	model = None
+
+	def __init__(self):
+		super(BaseSerializer, self).__init__()
+
+	def serialize(self, obj, many=False, pretty=False):
+		out = ""
+		if many:
+			out += "["
+			if len(obj) > 0:
+				for o in obj[:-1]:
+					out += self._serialize(o) + ", "
+				else:
+					out += self._serialize(obj[-1])
+			out += "]"
+		else:
+			out += self._serialize(obj)
+		return out
+
+	def _serialize(self, obj):
+		out = "{"
+		if obj != None and len(self.fields) > 0:
+			for field in self.fields[:-1]:
+				try:
+					func = self.__getattribute__(field)
+					if isinstance(func, BaseSerializer):
+						value = obj.__getattribute__(field)
+						value = func.model.query.filter_by(id=value).first()
+						value = func.serialize(value)
+					else:
+						value = obj.__getattribute__(func)(field)
+				except AttributeError, e:
+					try:
+						value = obj.__getattribute__(field)
+					except AttributeError, e:
+						raise e
+
+				if isinstance(value, str):
+					str_val = value
+				else:
+					str_val = str(value)
+					str_val = str_val.replace('\r', '\\r')
+					str_val = str_val.replace('\n', '\\n')
+					str_val = '"' + str_val + '"'
+
+				if isinstance(value, list):
+					value = list(value)
+
+				try:
+					if not isinstance(value, str):
+						str_val = json.dumps(value)
+				except TypeError, e:
+					if isinstance(value, list):
+						new_val = []
+						for v in value:
+							if isinstance(v, db.Model):
+								new_val.append(v.id)
+							else:
+								new_val.append(v)
+						str_val = json.dumps(new_val)
+
+				if field == self.fields[:-1]:
+					out += '"' + field + '":' + str_val
+				else:
+					out += '"' + field + '":' + str_val + ', '
+
+		out += "}"
+		return out
+
+	def load(self, string):
+		d = json.loads(string)
+		return model(**d)
