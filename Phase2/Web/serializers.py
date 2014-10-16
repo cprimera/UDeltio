ff9b0ad33a78f828e2cc451fd9e97d082c2d6642
Christopher Primerano <cprimera@mac.com>
2014-10-16 19:36:14 -0400

    Added basic serializers for each model

---
+++ Phase2/Web/serializers.py
@@ -0,0 +1,28 @@
+from models import *
+from utils import BaseSerializer
+
+
+
+class UserSerializer(BaseSerializer):
+	fields = ['id', 'username', 'first_name', 'last_name', 'email']
+	model = User
+
+class BoardSerializer(BaseSerializer):
+	fields = ['id', 'name', 'public']
+	model = Board
+
+class PostSerializer(BaseSerializer):
+	fields = ['id', 'user', 'board', 'creation_date', 'subject', 'content', 'important']
+	model = Post
+
+class TagSerializer(BaseSerializer):
+	fields = ['id', 'name']
+	model = Tag
+
+class SubscribersSerializer(BaseSerializer):
+	fields = ['id', 'user', 'board', 'read', 'write', 'admin', 'notify']
+	model = Subscribers
+
+class AssignedTagsSerializer(BaseSerializer):
+	fields = ['id', 'board', 'tag']
+	model = AssignedTags
