e5555e4d6622a9d686e8bc5746d73ceb81dc8f44
ycherenkova <layshu@gmail.com>
2014-10-23 19:22:02 -0400

    update page layout, add ng-switch to left submenu

--- Phase2/Web/static/js/app.js
+++ Phase2/Web/static/js/app.js
@@ -11,19 +11,33 @@ angular.module('UDeltio', ['ngRoute',])
 	    templateUrl: 'static/templates/board.html',
 	    controller: 'BoardCtrl',
 	  });
+	  $routeProvider.when('/board_settings/:param', {
+	    templateUrl: 'static/templates/board_settings.html',
+	    controller: 'BoardSettingsCtrl',
+	  });
 	  $routeProvider.otherwise({redirectTo: '/profile'});
 	}])
 
 
 	.controller('MainCtrl', ['$scope', function($scope) {
 		$scope.menu_template = 'static/templates/menu.html';
+		$scope.submenu_template = 'static/templates/submenu.html';
 	}])	
 
 	.controller('BoardCtrl', ['$scope', function($scope) {
+		$scope.cname = "board";
 	}])
 
 	.controller('ProfileCtrl', ['$scope', function($scope) {
+		$scope.cname = "profile";
+	}])
+
+	.controller('BoardSettingsCtrl', ['$scope', function($scope) {
+		$scope.cname = "board_settings";
 	}])
 
 	.controller('MenuCtrl', ['$scope', function($scope) {
-	}]);		
+	}]);
+
+
+
