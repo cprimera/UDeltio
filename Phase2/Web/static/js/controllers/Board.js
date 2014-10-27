c28ab521ccbd9586fb555d278d3515a2ced61597
Yasith Vidanaarachchi <yasith@tuxv.net>
2014-10-26 20:40:58 -0400

    Remove debugging symbols

--- Phase2/Web/static/js/controllers/Board.js
+++ Phase2/Web/static/js/controllers/Board.js
@@ -5,13 +5,9 @@ var BoardCtrl = angular.module('BoardCtrl', ['restangular']);
 
 BoardCtrl.controller('BoardCtrl', ['$scope', 'Restangular', '$routeParams', function($scope, Restangular, $routeParams) {
     $scope.cname = "board";
-    $scope.doofus = "Doofus";
     Restangular.one('boards', $routeParams['id']).get().then(function (board) {
         $scope.board = board;
     });
-    console.log("Board Controller running");
-    console.log($scope.board);
-    console.log($routeParams);
 }])
 
 
