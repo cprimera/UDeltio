655cc0ad6f968bb0ddc4e2e227b7e44c44e48490
Christopher Primerano <cprimera@mac.com>
2014-10-28 11:46:58 -0400

    Added Dredd testing for backend with API blueprints

---
+++ Test/Dredd/scripts/reset_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Removing Tables..."
+mysql -u udeltio --password=udeltio udeltio -e "drop table assigned_tags, post, subscribers, tag, token, client, user, board;"
+cd ../../Phase2/Web
+echo "Re-Creating Tables..."
+echo "from udeltio import db; from models import *; from oauth2.o_models import *; db.create_all();" | python2
+***REMOVED***
+***REMOVED***
+echo "Done"
