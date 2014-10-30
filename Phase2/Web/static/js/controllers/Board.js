e12474a4352f713855e85b86903cec43d166945b
ycherenkova <layshu@gmail.com>
2014-10-29 20:01:12 -0400

    add show-hide to new post

--- Phase2/Web/static/js/controllers/Board.js
+++ Phase2/Web/static/js/controllers/Board.js
@@ -4,31 +4,40 @@ var BoardCtrl = angular.module('BoardCtrl', ['restangular']);
 
 
 BoardCtrl.controller('BoardCtrl', ['$scope', 'Restangular', '$routeParams', function($scope, Restangular, $routeParams) {
-    $scope.cname = "board";
-    Restangular.one('boards', $routeParams['id']).get().then(function (board) {
-        $scope.board = board;
-    });
+	$scope.cname = "board";
+	Restangular.one('boards', $routeParams['id']).get().then(function (board) {
+		$scope.board = board;
+	});
 
-    Restangular.one('boards', $routeParams['id']).getList('posts').then(function (posts) {
-        $scope.posts = posts;
-    });
+	Restangular.one('boards', $routeParams['id']).getList('posts').then(function (posts) {
+		$scope.posts = posts;
+	});
 
-    $scope.save_post = function() {
-    	console.log($scope.newPost);
-    	$scope.newPost.important = false;
-    	$scope.newPost.board = $scope.board.id;
+
+	$scope.save_post = function() {
+		console.log($scope.newPost);
+		$scope.newPost.important = false;
+		$scope.newPost.board = $scope.board.id;
 		Restangular.all('posts').post('posts', $scope.newPost).then(function(postedData) {
-		    console.log("Success");
-		})
-    };
+			console.log("Success");
+			console.log($scope.newPost);
+		});
+		$scope.show_add_post_modal = !$scope.show_add_post_modal;
+	};
+
+
+	$scope.show_add_post_modal = false;
+	$scope.add_post = function() {
+		$scope.show_add_post_modal = !$scope.show_add_post_modal;
+	}
 
 }]);
 
 
 BoardCtrl.controller('BoardSettingsCtrl', ['$scope', function($scope) {
-    $scope.cname = "board_settings";
+	$scope.cname = "board_settings";
 }]);
 
 BoardCtrl.controller('CreateBoardCtrl', ['$scope', function($scope) {
-    $scope.cname = "create_board";
+	$scope.cname = "create_board";
 }]);
