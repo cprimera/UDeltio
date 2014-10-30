56cdf9b54070080b6edf8d2993c8fc894e3b2f32
ycherenkova <layshu@gmail.com>
2014-10-30 11:03:12 -0400

    add modal to be used on creation of a new post

--- Phase2/Web/static/js/controllers/Board.js
+++ Phase2/Web/static/js/controllers/Board.js
@@ -13,25 +13,19 @@ BoardCtrl.controller('BoardCtrl', ['$scope', 'Restangular', '$routeParams', func
 		$scope.posts = posts;
 	});
 
-
 	$scope.save_post = function() {
-		console.log($scope.newPost);
 		$scope.newPost.important = false;
 		$scope.newPost.board = $scope.board.id;
 		Restangular.all('posts').post('posts', $scope.newPost).then(function(postedData) {
 			console.log("Success");
-			console.log($scope.newPost);
+			$scope.newPost.subject = "";
+			$scope.newPost.content = "";
 		});
-		$scope.show_add_post_modal = !$scope.show_add_post_modal;
 	};
 
-
-	$scope.show_add_post_modal = false;
-	$scope.add_post = function() {
-		$scope.show_add_post_modal = !$scope.show_add_post_modal;
-	}
-
 }]);
+
+
 
 
 BoardCtrl.controller('BoardSettingsCtrl', ['$scope', function($scope) {
