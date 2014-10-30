637e1a2ba7b569686d2e9eb8f4b2ca6308a6736a
Yasith Vidanaarachchi <yasith@tuxv.net>
2014-10-30 15:40:24 -0400

    Move login button to navbar & Remove failing requests

--- Phase2/Web/static/js/controllers/Profile.js
+++ Phase2/Web/static/js/controllers/Profile.js
@@ -2,16 +2,23 @@
 
 var ProfileCtrl = angular.module('ProfileCtrl', ['restangular', 'ngCookies']);
 
-ProfileCtrl.controller('ProfileCtrl', ['$scope', 'Restangular', '$cookieStore', '$http', function($scope, Restangular, $cookieStore, $http) {
+ProfileCtrl.controller('ProfileCtrl', ['$scope', '$rootScope', 'Restangular', '$cookieStore', '$http', function($scope, $rootScope, Restangular, $cookieStore, $http) {
     $scope.cname = "profile";
     $scope.username = null;
     $scope.password = null;
 
+    $scope.loggedIn = function() {
+        return $cookieStore.get('token') != null;
+    }
+
     var getBoards = function() {
-        Restangular.all('boards').getList().then(function (boards) {
-            $scope.boards = boards;
-        });
+        if ($scope.loggedIn()) {
+            Restangular.all('boards').getList().then(function (boards) {
+                $scope.boards = boards;
+            });
+        }
     };
+
     getBoards();
 
     $scope.login = function() {
@@ -29,9 +36,9 @@ ProfileCtrl.controller('ProfileCtrl', ['$scope', 'Restangular', '$cookieStore', 
             });
     };
 
-    $scope.loggedIn = function() {
-        return $cookieStore.get('token') != null;
-    }
+    $scope.$on('logout', function(event) {
+        $scope.boards = null;
+    });
 
 }]);
 
