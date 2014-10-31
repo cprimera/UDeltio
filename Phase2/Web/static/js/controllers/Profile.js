2fab8b7022e1166bbc7494a6e4720dbd3e39c009
Will <william.g.armstrong@gmail.com>
2014-10-30 21:28:14 -0400

    hooked up the create board modal to the backend

--- Phase2/Web/static/js/controllers/Profile.js
+++ Phase2/Web/static/js/controllers/Profile.js
@@ -3,42 +3,65 @@
 var ProfileCtrl = angular.module('ProfileCtrl', ['restangular', 'ngCookies']);
 
 ProfileCtrl.controller('ProfileCtrl', ['$scope', '$rootScope', 'Restangular', '$cookieStore', '$http', function($scope, $rootScope, Restangular, $cookieStore, $http) {
-    $scope.cname = "profile";
-    $scope.username = null;
-    $scope.password = null;
+		$scope.cname = "profile";
+		$scope.username = null;
+		$scope.password = null;
 
-    $scope.loggedIn = function() {
-        return $cookieStore.get('token') != null;
-    }
+		$scope.loggedIn = function() {
+				return $cookieStore.get('token') != null;
+		}
 
-    var getBoards = function() {
-        if ($scope.loggedIn()) {
-            Restangular.all('boards').getList().then(function (boards) {
-                $scope.boards = boards;
-            });
-        }
-    };
+		var getBoards = function() {
+				if ($scope.loggedIn()) {
+						Restangular.all('boards').getList().then(function (boards) {
+								$scope.boards = boards;
+						});
+				}
+		};
 
-    getBoards();
+		getBoards();
 
-    $scope.login = function() {
-        var auth = Restangular.oneUrl('auth', 'http://udeltio.com/oauth2').customPOST({
-            "client_id": "***REMOVED***",
-            "client_secret": "***REMOVED***",
-            "grant_type": "password",
-            "username": $scope.username,
-            "password": $scope.password
-        },
-        "access_token", {}, {}).then(function(response){
-                $cookieStore.put('token', response.access_token);
-                $http.defaults.headers.common['Authorization']  = 'Bearer ' + response.access_token;
-                getBoards();
-            });
-    };
+		$scope.login = function() {
+				var auth = Restangular.oneUrl('auth', 'http://udeltio.com/oauth2').customPOST({
+						"client_id": "***REMOVED***",
+						"client_secret": "***REMOVED***",
+						"grant_type": "password",
+						"username": $scope.username,
+						"password": $scope.password
+		},
+				"access_token", {}, {}).then(function(response){
+						$cookieStore.put('token', response.access_token);
+						$http.defaults.headers.common['Authorization']  = 'Bearer ' + response.access_token;
+						getBoards();
+				});
+		};
 
-    $scope.$on('logout', function(event) {
-        $scope.boards = null;
-    });
+		$scope.newBoard = {"name":"", "public":true};
+
+
+		$scope.saveBoard = function() {
+				Restangular.one('boards').customPOST($scope.newBoard).then(function(postedData) {
+						$scope.newBoard = {"name":"", "public":true};
+						$scope.boards.push(postedData);
+						$("#createBoardModal").modal("toggle");
+				});		
+
+
+		}
+
+		$scope.newuser = {'username': '', 'read': false, 'write': false, 'admin': false};
+
+		$scope.saveUser = function() {
+				return Restangular.one('boards', $routeParams['id']).customPOST($scope.newuser, 'users').then(function(data) {
+						$scope.users.push(data);
+						$scope.newuser = {'username': '', 'read': false, 'write': false, 'admin': false};
+				});
+		};
+
+
+		$scope.$on('logout', function(event) {
+				$scope.boards = null;
+		});
 
 }]);
 
