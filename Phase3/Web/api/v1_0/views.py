9e8c6f4ff0c65591b39c62fc3fda7d96d2647b94
Christopher Primerano <cprimera@mac.com>
2014-11-14 10:32:25 -0500

    When a user creates a new board they automatically favorite it

--- Phase3/Web/api/v1_0/views.py
+++ Phase3/Web/api/v1_0/views.py
@@ -99,7 +99,7 @@ def boards_collection():
 		try:
 			db.session.commit()
 			board = Board.query.filter_by(name=request.json.get("name", None)).first_or_404()
-			subscriber = Subscribers(board=board.id, user=user_from_oauth().id, read=False, write=False, admin=True, notify=False, favorite=False)
+			subscriber = Subscribers(board=board.id, user=user_from_oauth().id, read=False, write=False, admin=True, notify=False, favorite=True)
 			db.session.add(subscriber)
 			db.session.commit()
 		except IntegrityError:
