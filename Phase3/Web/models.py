87e17579e3c135490cab4194c9247b55d9af9403
Christopher Primerano <cprimera@mac.com>
2014-11-14 11:46:29 -0500

    Added endpoints to retrieve, add and delete tags from a board

--- Phase3/Web/models.py
+++ Phase3/Web/models.py
@@ -126,7 +126,7 @@ class Tag(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 
-	name = db.Column(db.String(100))
+	name = db.Column(db.String(100), unique=True)
 
 	def __init__(self, name):
 		self.name = name
